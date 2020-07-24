8f8a0e8920780e8746d450c144d454a4bce93962
Aissaoui Ahmed <aissaoui@softvenue.net>
2020-07-24 02:32:42 +0100

    Create hasAccent
    Signed-off-by: Aissaoui Ahmed <aissaoui@softvenue.net>

---
+++ src/has_accent.js
@@ -0,0 +1,28 @@
+import letters from '../letters.js';
+
+/**
+ *
+ * @function hasAccent
+ * @param {String} string
+ * @returns {Boolean} Boolean.
+ * @description Check accent on string (true || false).
+ */
+const hasAccent = (string) => {
+  const letter = string.split('');
+  const result = letter.some((x) => {
+    let state = false;
+    for (let i = 0; i < Object.keys(letters).length; i += 1) {
+      const accent = Object.keys(letters)[i];
+      const letterCode = x.charCodeAt().toString();
+      if (accent === letterCode) {
+        state = true;
+        break;
+      }
+    }
+    return state;
+  });
+  console.log(result);
+  return result;
+};
+
+export default hasAccent;
